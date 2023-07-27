c40624434ff3f504ef957035a578746c468a1a38
Ali Nosrati <alinosrati1995@gmail.com>
2023-07-27 14:59:02 +0330

    Add Subtitle Conversion

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
 from flask import Flask
 from os import listdir
+import webvtt
 
 app = Flask(__name__)
 
 video_files = [
     f for f in listdir("static/videos") if f.endswith(".mkv") or f.endswith("mp4")
 ]
+
+srt_files = [
+    f for f in listdir("static/videos") if f.endswith("srt")
+]
+
+for f in srt_files:
+    webvtt.from_srt("static/videos/"+f).save()
+
 sub_files = [f for f in listdir("static/videos") if f.endswith(".vtt")]
 
 
@@ -15,7 +24,7 @@ def hello():
     html += '<video src="/static/videos/{}" controls>'.format(video_files[0])
 
     for sub in sub_files:
-        html += '<track label="English" kind="subtitles" srclang="en" src="/static/videos/{}" />'.format(
+        html += '<track label="English" kind="subtitles" srclang="en" src="/static/videos/{}" default/>'.format(
             sub
         )
     html += "</video>"
